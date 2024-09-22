d00afd1fa96926e292c031766aea82a539520b90
Cubita238 <texnowjueperra@gmail.com>
2024-09-22 21:02:00 +0300

    iniciado

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['6287711461894']
+global.botname = '𝐂•𝟏𝟑𝟕¿ 𝐒𝐜𝐫𝐢𝐩𝐭'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "𝐂•𝟏𝟑𝟕¿ 𝐒𝐜𝐫𝐢𝐩𝐭"
+global.sticker2 = "🕸️"
